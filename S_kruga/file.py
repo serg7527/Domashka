539b5be8ca9ee6b8cccc97c04b911927f60e155a
Searhei <156844838+serg7527@users.noreply.github.com>
2024-01-27 01:19:36 +0300

    ver 1.0

---
+++ S_kruga/file.py
@@ -0,0 +1,14 @@
+print("Площадь вписанного в треугольник круга:")
+a = 5
+b = 7
+c = 6
+A = 90
+p = ((a+b+c)/2)
+print("Полупериметр:", (p))
+import math 
+Radius = (p-a)*(math.tan(A/2))
+def circle(Radius):  
+    area = Radius** 2 * math.pi 
+    return area
+#Radius = float(input("Введите радиус круга: "))
+print("Площадь круга: ", circle(Radius))
