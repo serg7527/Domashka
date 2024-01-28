0e89a0ffe1f3bb90d11bcf452513c2c110247052
Searhei <156844838+serg7527@users.noreply.github.com>
2024-01-28 23:47:16 +0300

    y=

---
+++ S_kruga/formuls.py
@@ -0,0 +1,6 @@
+a = float(input("Введите a:"))
+b = float(input("Введите b:"))
+x = float(input("Введите x:"))
+import math
+y = (a**a/3)+((a**a)+4)/b+((math.sqrt(a**a)+4))/4+((math.sqrt(a**a)+4)**3)/4
+print(y)
