44eb2c81c0ed447a84da1764ee7a93187fe2dd91
Searhei <156844838+serg7527@users.noreply.github.com>
2024-01-30 19:01:22 +0300

    wer3

--- S_kruga/file_new.py
+++ S_kruga/file_new.py
@@ -1,3 +1,4 @@
+import math
 print("Площадь вписанного в треугольник круга:")
 a = float(input("Введите длину стороны a:"))
 b = float(input("Введите длину стороны b:"))
@@ -5,7 +6,6 @@ c = float(input("Введите длину стороны c:"))
 A = float(input("Введите угол A в градусах:"))
 p = ((a+b+c)/2)
 print("Полупериметр:", (p))
-import math 
 Radius = (p-a)*(math.tan(A/2))
 def circle(Radius):  
     area = Radius** 2 * math.pi 
